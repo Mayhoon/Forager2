174434d38a862dea8c5c0f0658198fa238ae879d
Mayhoon <fell2ratte@t-online.de>
2020-04-21 22:37:21 +0200

    added new network data

--- core/src/networking/NetworkData.java
+++ core/src/networking/NetworkData.java
@@ -7,13 +7,17 @@ import com.badlogic.gdx.math.Vector2;
 public class NetworkData {
     public Vector2 position;
     public Direction direction;
+    public Boolean moving;
+    public float movementSpeed;
 
     //Animations
     public AnimationName animation;
 
     public NetworkData() {
-        position = new Vector2(100, 0);
+        position = new Vector2(0, 0);
         direction = Direction.NONE;
         animation = AnimationName.IDLE_SWORD_NOT_DRAWN;
+        moving = false;
+        movementSpeed = 0f;
     }
 }
--- core/src/player/Player.java
+++ core/src/player/Player.java
@@ -16,14 +16,14 @@ public class Player {
     private GamePadInput gamePadInput;
     public PlayerInputAnimationMapper playerInputAnimationMapper;
     private Boolean moving;
-    float previousAmount;
+    float prreviousAmount;
 
     public Player(Entity entity, ServerClientWrapper wrapper, Camera camera) {
         this.entity = entity;
         this.wrapper = wrapper;
         this.camera = camera;
         this.moving = false;
-        this.previousAmount = 0f;
+        this.prreviousAmount = 0f;
         this.playerInputAnimationMapper = new PlayerInputAnimationMapper(wrapper, entity);
         this.gamePadInput = new GamePadInput(this);
         this.position = new Vector2(0, 0);
@@ -31,21 +31,17 @@ public class Player {
     }
 
     public void render(SpriteBatch batch) {
-        if (moving == true && wrapper.ownData().direction.equals(Direction.LEFT)) {
-            wrapper.ownData().position.x += previousAmount;
+        if (wrapper.ownData().moving == true && wrapper.ownData().direction.equals(Direction.LEFT)) {
+            wrapper.ownData().position.x += wrapper.ownData().movementSpeed;
+            System.out.println(wrapper.ownData().movementSpeed);
             playerInputAnimationMapper.update(batch);
-
-        } else if (moving == true && wrapper.ownData().direction.equals(Direction.RIGHT)) {
-            wrapper.ownData().position.x += previousAmount;
+        } else if (wrapper.ownData().moving == true && wrapper.ownData().direction.equals(Direction.RIGHT)) {
+            wrapper.ownData().position.x += wrapper.ownData().movementSpeed;
             playerInputAnimationMapper.update(batch);
-
         } else {
             playerInputAnimationMapper.update(batch);
         }
-        sendPlayerInformation();
-    }
 
-    private void sendPlayerInformation() {
         if (entity.equals(Entity.Opponent)) {
             position = wrapper.ownData().position;
         } else {
@@ -59,20 +55,20 @@ public class Player {
         //Find out to which direction the player faces
         if (amount < -0.045f) {
             wrapper.ownData().direction = Direction.LEFT;
-            if (previousAmount + (Math.abs(amount)) >= 0) {
-                moving = true;
-                previousAmount = 1.5f * amount;
+            if (prreviousAmount + (Math.abs(amount)) >= 0) {
+                wrapper.ownData().moving = true;
+                wrapper.ownData().movementSpeed = 1.5f * amount;
             }
         } else if (amount > 0.045f) {
             wrapper.ownData().direction = Direction.RIGHT;
-            if ((amount - previousAmount) >= 0) {
-                moving = true;
-                previousAmount = 1.5f * amount;
+            if ((amount - prreviousAmount) >= 0) {
+                wrapper.ownData().moving = true;
+                wrapper.ownData().movementSpeed = 1.5f * amount;
             }
         } else {
             amount = 0;
             wrapper.ownData().direction = Direction.NONE;
-            moving = false;
+            wrapper.ownData().moving = false;
         }
         wrapper.ownData().position = position;
     }
